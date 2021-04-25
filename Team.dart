8e80780acf22d651d1ca5922c0c8eecbf538a7d2
Ahmed Shawky <59096080+AhmedShawkyAhmed@users.noreply.github.com>
2021-04-25 22:34:38 +0200

    Update

--- Team.dart
+++ Team.dart
@@ -1,5 +1,19 @@
 import 'dart:io';
 
 main() {
-  
+  var problem = stdin.readLineSync();
+  int num = int.parse(problem!);
+  int counter = 0;
+  for (int i = 0; i < num; i++) {
+    int total = 0;
+    for (int q = 0; q < 3; q++) {
+      var ans = stdin.readLineSync();
+      int answer = int.parse(ans!);
+      total += answer;
+    }
+    if (total >= 2) {
+      counter++;
+    }
+  }
+  print(counter);
 }
